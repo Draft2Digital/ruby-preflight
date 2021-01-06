50b768b0e35a4fa44e35bf42f73158b7cc1b042c
Carlos <velez.carlos.a@gmail.com>
2021-01-06 16:39:08 -0600

    PRINT-160 - edit library to resolve syntax issues (removing '.new') and add profile pdfx1a2003 .

---
+++ lib/main.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
+require "preflight"
+
+preflight = Preflight::Profiles::PDFX1A_2003.new
+
+if ARGV.length == 0
+  puts "ERROR: filepath required"
+  exit
+end
+
+filename = ARGV[0]
+
+File.open(filename, "rb") do |file|
+  puts preflight.check(file).inspect
+end
